53e486115520866de1d629d400e7018224c0b749
Sarah <102232226+SarahHomberg@users.noreply.github.com>
2022-03-24 15:01:59 +0100

    Spanish added

--- frontend/src/views/Microphone.vue
+++ frontend/src/views/Microphone.vue
@@ -5,6 +5,7 @@
             <select name="lang" @change="onChange($event)" class="custom-select">
                 <option value="de-DE" selected>German</option>
                 <option value="en-US">English</option>
+                <option value="es-ES">Espanol</option>
             </select>
         </div>
         <br>
